7a71ec9cd8d30cadd736009ff8995f0ecb83a659
Angelo Ponce25 <Angeloponce25@gmail.com>
2024-02-08 13:21:36 -0500

    A: Cambios en el inicio post login

--- Vista/modulos/contenido.php
+++ Vista/modulos/contenido.php
@@ -3,8 +3,8 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Blank page
-        <small>it all starts here</small>
+        Dashboard
+        <small>Control panel</small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -12,7 +12,55 @@
         <li class="active">Blank page</li>
       </ol>
     </section>
+<!-- Main content -->
+<section class="content">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-lg-4 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>150</h3>
 
+              <p>Compras</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>            
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-4 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+              <p>Ventas</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>            
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-4 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>44</h3>
+
+              <p>Pacientes</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>            
+          </div>
+        </div>
+      </div>
+      <!-- /.row -->
+
+    </section>
     <!-- Main content -->
     <section class="content">
 
